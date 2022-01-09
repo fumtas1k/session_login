eea961e8063d9bb6b4278d155454236ac2d9400b
fumtas1k <fumta01a7@gmail.com>
2022-01-09 18:56:23 +0900

    add:bcrypt導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: /\A[\w+\-.]+@[\w\-.]+\.[a-z]+\z/i},
-                    uniquness: true
+                    uniqueness: true
   before_validation {email.downcase!}
+
+  has_secure_password
+  validates :password, length: {minimum: 6}
 end
