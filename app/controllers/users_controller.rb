82ea485518d1dfe41da692e3aad31ec0c2113816
fumtas1k <fumta01a7@gmail.com>
2022-01-10 11:51:58 +0900

    improve:createからのリダイレクトの記載を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id), notice: "登録しました!"
+      redirect_to @user, notice: "登録しました!"
     else
       render :new
     end
